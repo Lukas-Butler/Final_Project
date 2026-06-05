f907112450623676a3caf6dbef4bcc47ece70944
KalebSparvierWendler <kaleb.sparvierwend@rbe.sk.ca>
2026-06-05 15:11:08 -0600

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -1,2 +1,23 @@
+import player as p
+import Room as r
+import random as rand
 
-    
+
+
+class Enemy:
+    def __init__(self, species, location, r):
+        self.species = species
+        self.location = location
+        self.map = r
+
+    def move (self, p.location, p.direction, row, col, map):
+        if p.location == p.direction == "up":
+            row[0] + rand.randrange(0,2) or col[0] + rand.randrange(0,2)
+        else:
+            print("The enemy decided to not move")
+        elif p.location == p.direction == "down":
+            
+        elif p.location == p.direction == "right":
+            
+        elif p.location == p.direction == "left":
+            
