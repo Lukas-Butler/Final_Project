a0efa035ed6ed981abbe389b9af0b08f9e674f59
Lukas-Butler <lukas.butler@rbe.sk.ca>
2026-06-09 14:39:30 -0600

    nothing

--- player.py
+++ player.py
@@ -19,7 +19,6 @@ class Player:
         self.location = Engine_Room.getStart()
         return grid
 
-
     def movement(self):
         print("which direction would you like to go:\n" \
               " - up\n" \
