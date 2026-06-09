d8997ae4b95210b3df3afaa758502089aaf682f1
Kaleb Sparvier-Wendler <kaleb.sparvierwend@rbe.sk.ca>
2026-06-09 14:35:09 -0600

    enemies update

--- Enemies.py
+++ Enemies.py
@@ -10,14 +10,18 @@ class Enemy:
         self.location = location
         self.map = r
 
+    def enemy_move(self, row, col):
+        if rand.randrange(0,2) == 0:
+            row[0] + rand.randrange(0,2) 
+        elif rand.randrange(0,1) == 1:
+            col[0] + rand.randrange(0,2)
+
     def move (self, p.location, p.direction, row, col, map):
         if p.location == p.direction == "up":
-            row[0] + rand.randrange(0,2) col[0] + rand.randrange(0,2)
-        else:
-            print("The enemy decided to not move")
+            self.enemy_move(row, col)
         elif p.location == p.direction == "down":
-            
+            self.enemy_move(row, col)
         elif p.location == p.direction == "right":
-            
+            self.enemy_move(row, col)
         elif p.location == p.direction == "left":
-            
+            self.enemy_move(row, col)
